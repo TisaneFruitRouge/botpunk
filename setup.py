b6c0f9092dfb1a7b4bf79341553c285a0de9c1ba
TisaneFruitRouge <34172800+TisaneFruitRouge@users.noreply.github.com>
2020-08-29 22:20:35 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+
+setup(
+  name = 'jetpunkbot',         
+  packages = ['JETPUNKBOT'],   
+  version = '0.1',      
+  license='MIT',        
+  description = 'A bot that solves quizzes for you',   
+  author = 'Vincent WENDLING',                   
+  author_email = 'vincent.wendling@lilo.org',     
+  url = 'https://github.com/TisaneFruitRouge/jetpunk-bot',   
+  download_url = 'https://github.com/TisaneFruitRouge/jetpunk-bot/archive/v_01.tar.gz',    
+  keywords = ['BOT', 'QUIZZES', 'JETPUNK'],   
+  install_requires=[            
+          'selenium',
+          'beautifulsoup4',
+          'requests',
+      ],
+  classifiers=[
+    'Development Status :: 4 - Beta',      
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3.8',
+  ],
+)
