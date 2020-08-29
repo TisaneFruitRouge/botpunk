fea11ab0d005bb58d41d3a36afea4f7778bcbb8e
TisaneFruitRouge <34172800+TisaneFruitRouge@users.noreply.github.com>
2020-08-29 22:43:26 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from distutils.core import setup
 
 setup(
-  name = 'jetpunkbot',         
-  packages = ['JETPUNKBOT'],   
+  name = 'punkbot',         
+  packages = ['PUNKBOT'],   
   version = '0.1',      
   license='MIT',        
   description = 'A bot that solves quizzes for you',   
   author = 'Vincent WENDLING',                   
   author_email = 'vincent.wendling@lilo.org',     
   url = 'https://github.com/TisaneFruitRouge/botpunk',   
-  download_url = 'https://github.com/TisaneFruitRouge/botpunk/archive/v_01.tar.gz',    
+  download_url = 'https://github.com/TisaneFruitRouge/botpunk/archive/v_02.tar.gz',    
   keywords = ['BOT', 'QUIZZES', 'JETPUNK'],   
   install_requires=[            
           'selenium',
